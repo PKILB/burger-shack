8fbf8886c54727c5f820ea77f8960c7d9d552037
Payton Boone <paytonboone30@gmail.com>
2023-02-13 15:45:13 -0700

    first commit

---
+++ server/db/FakeDb.js
@@ -0,0 +1,19 @@
+class FakeDb {
+
+    burgers = [{
+        id: '2747dh488',
+        name: 'Cheeseburger',
+        emoji: '🍔'
+    }, {
+        id: '47494u5',
+        name: 'Vege-Burger',
+        emoji: '🥬'
+    }
+]
+}
+
+
+
+
+
+export const fakeDb = new FakeDb()
